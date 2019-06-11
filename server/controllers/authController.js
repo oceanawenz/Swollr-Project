6d2045471d2887241768574c4fe599ded656c80e
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-06-11 09:48:44 -0700

    auth endpoinst/controller done and works

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -11,9 +11,9 @@ module.exports = {
                 const saltRounds = 12;
                 bcrypt.genSalt(saltRounds).then(salt => {
                     bcrypt.hash(password, salt).then(hashedPassword => {
-                        db.register([user_name, hashedPassword, email]).then((createdUser) => {
+                        db.register_user([user_name, hashedPassword, email]).then((createdUser) => {
                             req.session.user = createdUser[0]
-                            res.statusMessage = "Register Successful"
+                            // res.statusMessage = "Register Successful"
                             res.status(200).send(req.session.user)
                         })
                     })
@@ -23,10 +23,10 @@ module.exports = {
     },
 
     login: (req, res, next) => {
-        const { username, password } = req.body;
+        const { user_name, password } = req.body;
         const db = req.app.get('db');
          //if the user attempts to login with username, but their username is not found
-        db.check_if_user_exists(username).then((userFound) => {
+        db.check_if_user_exists(user_name).then((userFound) => {
              if(!userFound[0]) {
                  //send status of incorrect email/password
                  res.status(200).send('Incorrect email/password')
@@ -34,9 +34,9 @@ module.exports = {
                  bcrypt.compare(password, userFound[0].password).then(matchedPassword => {
                      //if there is a matched password
                      if(matchedPassword) {
-                         const {username, email, user_id } = userFound[0]
+                         const {user_name, email, user_id } = userFound[0]
                          // send back the user_name email and user_id from userFound[0]
-                         req.session.user = {username, email, user_id}
+                         req.session.user = {user_name, email, user_id}
                          res.status(200).send(req.session.user);
                      } else {
                          res.status(200).send('Incorrect Password. Please try again')
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const massive = require('massive');
+const session = require('express-session');
+const app = express();
+app.use(express.json());
+const {userInfo, login, register, logout} = require('./controllers/authController');
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SCECRET} = process.env
 
 const port = SERVER_PORT || 4000;
 
@@ -12,5 +15,31 @@ massive(CONNECTION_STRING).then(db => {
     app.set("db", db);
     console.log("db connected")
 })
+
+app.use(session({
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    resave: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 14 //2 week cookie
+    }
+}))
+
+//auth endpoints
+app.get('/auth/user', userInfo);
+app.get('/auth/logout', logout);
+app.post('/auth/register', register);
+app.post('/auth/login', login);
+
+//exercise endpoints
+app.get("/api/exercises");
+app.post("/api/exercises");
+//update
+app.put("/api/exercises/:id");
+app.delete("/api/exercises/:id")
+
+
+
+
 
 app.listen(port, () => console.log(`port running on ${port}`))

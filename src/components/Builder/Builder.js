6b85aab223fc63e7e41db54ca5fa0c05deff5e31
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-06-15 11:22:59 -0700

    working on update functionality

--- src/components/Builder/Builder.js
+++ src/components/Builder/Builder.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { connect} from 'react-redux';
+// import { saveUser } from '../../dux/reducer';
 import '../Builder/Builder.css'
 import List from '../List/List';
 import axios from 'axios';
@@ -13,33 +14,59 @@ class Builder extends Component {
         this.state = {
             exercises: [],
             reps: 0,
-            sets: 0,
-            prevState: 0
+            sets: 0
         }
         this.componentDidMount = this.componentDidMount.bind(this)
-        this.addToSet = this.addToSet.bind(this)
+        this.updateExercise = this.updateExercise.bind(this)
+        this.add = this.add.bind(this)
+        this.addToReps = this.addToReps.bind(this)
+        this.subtract = this.subtract.bind(this)
+        this.subtractReps = this.subtractReps.bind(this)
     }
 
 
-addToSet() {
+add() {
     this.setState({
         sets: this.state.sets + 1
     }) 
 }
 
+subtract() {
+    if(this.state.sets > 0) {
+        this.setState(prevState => ({sets: prevState.sets - 1}))
+    }
+}
 
-componentDidMount() {
-        axios.get(`/api/exercises`).then(exercises => {
-            this.setState({
-                exercises: exercises.data
-            })
-        })
+subtractReps() {
+    if(this.state.reps > 0) {
+        this.setState(prevState => ({reps: prevState.reps - 1}))
+    }
+}
+
+addToReps() {
+    this.setState({
+        reps: this.state.reps + 1
+    }) 
 }
 
 
 
 
+componentDidMount() {
+    axios.get(`/api/exercises`).then(exercises => {
+        this.setState({
+            exercises: exercises.data
+        })
+    })
+}
 
+updateExercise(id, sets, reps) {
+    axios.put(`/api/exercise/${id}`, {sets, reps}).then(exercise => {
+        this.setState({
+            exercises : exercise.data
+        })
+    })
+}
 
     render(){
         const mappedExercises = this.state.exercises.map(exercise => {
@@ -50,6 +77,18 @@ componentDidMount() {
                     <div>{exercise.instructions}</div>
                     <div>{exercise.sets}</div>
                     <div>{exercise.reps}</div>
+
+                    {/* <div>
+                        <button onClick={this.add}>+</button>
+                        <div>{exercise.sets}</div>
+                        <button onClick={this.subtract}>-</button>
+                    </div>
+                    <div>
+                        <button onClick={this.addToReps}>+</button>
+                        <div>{exercise.reps}</div>
+                        <button onClick={this.subtractReps}>-</button>
+                    </div> */}
+                    
                 </div>
             )
         })
@@ -57,10 +96,16 @@ componentDidMount() {
             <h3>Exercises</h3>
                 {mappedExercises}
                 <button>Add to list</button> 
-                <div>
-                    <h1>Sets : {this.state.sets}</h1>
-                    <button onClick={this.addToSet}>+</button>
-                </div>
+                    <div>
+                        <h1>Sets {this.state.sets}</h1>
+                        <button onClick={this.add}>+</button>
+                        <button onClick={this.subtract}>-</button>
+                    </div>
+                    <div>
+                        <h1>Reps {this.state.reps}</h1>
+                        <button onClick={this.addToReps}>+</button>
+                        <button onClick={this.subtractReps}>-</button>
+                    </div>
                 
                 <div>
                 <List/>
